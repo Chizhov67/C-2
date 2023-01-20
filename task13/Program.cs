cd1f3e1fccbe7de9a024f2e7b7848ab25f3d3bb8
Andrey <chizhov67@gmail.com>
2023-01-20 22:17:15 +0300

    Task13

---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿int N, thirdNum;
+
+Console.Write("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+			if (N < 100)
+			{
+				Console.WriteLine("Третьей цифры нет");
+			}
+
+			else
+			{
+				while(N > 999)
+				{
+					N = N / 10;
+				}
+
+				thirdNum = N % 10;
+				Console.WriteLine(thirdNum);
+			}
+
+			Console.ReadLine();
+        
