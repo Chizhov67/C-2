a17e0ddbad070e97102ab44f2f79f333c902d0d1
Andrey <chizhov67@gmail.com>
2023-01-20 22:24:03 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿int N;
+Console.Write("Введите цифру дня недели: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+if (N >= 1 && N <= 5)
+			{
+				Console.WriteLine("нет");
+			}
+
+			else if (N >= 6 && N <= 7)
+			{
+				Console.WriteLine("да");
+			}
+
+			else
+			{
+				Console.WriteLine("Такого дня не существует");
+			}	
+
+			Console.ReadLine();
+		
